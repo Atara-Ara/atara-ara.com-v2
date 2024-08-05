d65501a5ede1f7a477f2b678d47ad05e69940e62
Chandler Chilvers <cwchilvers@gmail.com>
2024-08-05 12:02:34 -0700

    Updates.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Navbar from '../../lib/components/Navbar.svelte';
 	import Footer from '../../lib/components/Footer.svelte';
-	import '../../app.sass';
 </script>
 
 <div class="app">
