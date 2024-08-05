b3dc9bb3083e9faf5d7fe79d84270c30e55c666a
Chandler Chilvers <cwchilvers@gmail.com>
2024-08-04 22:28:04 -0700

    Modified layouts. Added footer.

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Navbar from '../../lib/components/Navbar.svelte';
+	import Footer from '../../lib/components/Footer.svelte';
+	import '../../app.sass';
+</script>
+
+<div class="app">
+	<Navbar />
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,36 @@
+<script>
+	import Footer from '../lib/components/Footer.svelte';
+</script>
+
 <svelte:head>
 	<title>Atara Ara</title>
 	<meta name="description" content="Offical website for Atara Ara." />
 </svelte:head>
 
-<section>
+<main>
 	<h1>Atara Ara</h1>
-</section>
+	<nav>
+		<ul>
+			<li>
+				<a href="/music">Music</a>
+			</li>
+			<li>
+				<a href="/shows">Shows</a>
+			</li>
+			<li>
+				<a
+					href="https://atara-ara.bandcamp.com/"
+					target="_blank"
+					rel="noopener noreferrer">Store</a
+				>
+			</li>
+			<li>
+				<a href="/gallery">Gallery</a>
+			</li>
+		</ul>
+	</nav>
+	<Footer />
+</main>
 
 <style>
 </style>
